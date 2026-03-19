1025693998b819e2e5a8e6d95fffa6e5ffb72d9c
halleyre <me@halleyre.dev>
2026-03-19 12:40:07 +1100

    WIP #5 #6 app handler

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,62 @@
 use winit::{
-    event_loop::EventLoop,
-    window::Window,
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
+    window::{Window, WindowAttributes, WindowId},
 };
 
-use winit::application::ApplicationHandler;
-use winit::event::WindowEvent;
-use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
-use winit::window::{Window, WindowId};
+enum WindowStatus {
+    Inactive(WindowAttributes),
+    Active(Window),
+}
 
-#[derive(Default)]
 struct Graphics {
-    window: Option<Window>,
+    window: WindowStatus,
 }
 
 impl ApplicationHandler for Graphics {
+
     // also called on first startup
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        if !self.window
-        let mut window_attr = Default::default()
-            .with_title("stowball");
-        #[cfg(target_arch = "wasm32")]
-        {
-            use wasm_bindgen::JsCast;
-            let canvas = web_sys::window()
-                .and_then(|w| w.document()?
-                    .get_element_by_id("canvas")?
-                    .dyn_into::<web_sys::HtmlCanvasElement>()
-                    .ok()
-                );
-            window_attr = window_builder.with_canvas(canvas);
-        }
-
-        let window = event_loop.create_window(window_attr).unwrap();
+        use WindowStatus::*;
+        if let Inactive(win_attr) = &self.window {
+            // Option::take and Box would be more performant but .clone() is cleaner code
+            self.window = Active(event_loop.create_window(win_attr.clone()).unwrap());
+        };
     }
 
-    fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent) {
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
         match event {
             WindowEvent::CloseRequested => {
-                println!("The close button was pressed; stopping");
                 event_loop.exit();
             },
             WindowEvent::RedrawRequested => {
-                // Redraw the application.
-                //
-                // It's preferable for applications that do not render continuously to render in
-                // this event rather than in AboutToWait, since rendering in here allows
-                // the program to gracefully handle redraws requested by the OS.
-
-                // Draw.
-
-                // Queue a RedrawRequested event.
-                //
-                // You only need to call this if you've determined that you need to redraw in
-                // applications which do not always need to. Applications that redraw continuously
-                // can render here instead.
-                self.window.as_ref().unwrap().request_redraw();
             }
             _ => (),
         }
     }
 }
 
-fn main() {
-    let event_loop = EventLoop::new().unwrap();
-
-    // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
-    // dispatched any events. This is ideal for games and similar applications.
-    event_loop.set_control_flow(ControlFlow::Poll);
-
-    // ControlFlow::Wait pauses the event loop if no events are available to process.
-    // This is ideal for non-game applications that only update in response to user
-    // input, and uses significantly less power/CPU time than ControlFlow::Poll.
-    event_loop.set_control_flow(ControlFlow::Wait);
-
-    let mut app = App::default();
-    event_loop.run_app(&mut app);
-}
-
 pub fn main() {
     let event_loop = EventLoop::new().unwrap();
-
     event_loop.set_control_flow(ControlFlow::Wait);
 
-    
+    let mut win_attr = WindowAttributes::default()
+       .with_title("stowball");
 
+    #[cfg(target_arch = "wasm32")]
+    {
+        use wasm_bindgen::JsCast;
+        use winit::platform::web::WindowAttributesExtWebSys;
+        let canvas = web_sys::window()
+            .and_then(|w| w.document()?
+                .get_element_by_id("canvas")?
+                .dyn_into::<web_sys::HtmlCanvasElement>()
+                .ok()
+            );
+        win_attr = win_attr.with_canvas(canvas);
+    }
 
+    let mut app = Graphics { window: WindowStatus::Inactive(win_attr) };
+    event_loop.run_app(&mut app).unwrap();
 }
